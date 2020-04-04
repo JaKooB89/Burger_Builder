046b903bdd451faf5ab6405a504ec604173344e6
JakubC <jakub@clydewire.com>
2020-04-04 18:05:36 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.sass';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Burger Builder</h1>
+          <p>ReactJS Application</p>
+          <code>npm start</code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
